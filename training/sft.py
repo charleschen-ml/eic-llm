d21a75d36c6bcca18ae386c6cebb1a6405062e36
charleschen-ml <charleschen0305@gmail.com>
2025-06-17 16:00:32 -0700

    fix debug print

--- training/sft.py
+++ training/sft.py
@@ -113,7 +113,7 @@ def main(script_args, training_args, model_args):
     dataset = load_dataset(script_args.dataset_name, name=script_args.dataset_config)
     dataset = dataset.map(sft_preprocess)
     print("preprocessed dataset")
-    print(dataset.head(1)) # debug print
+    print(dataset['text'][0]) # debug print
     dataset = dataset.select(range(3)) # truncate for pipeline debug
 
     ################
