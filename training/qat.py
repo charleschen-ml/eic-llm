01dcadc5ae004163356b8ef66191386d04e842b4
charleschen-ml <charleschen0305@gmail.com>
2025-06-25 14:51:24 -0700

    move precomputed weights to the same device as the layer itself

--- training/qat.py
+++ training/qat.py
@@ -93,8 +93,9 @@ def patch_linear_forward_with_switchable_quantization(model, bit_widths=[4, 8]):
             module._quantized_weights = {}  # e.g., {4: tensor, 8: tensor}
 
             # Precompute quantized weights
+            device = module.weight.device # Ensure precomputed weights are on the same device as the layer itself
             for b in bit_widths:
-                module._quantized_weights[b] = quantize_tensor(module.weight.data, num_bits=b)
+                module._quantized_weights[b] = quantize_tensor(module.weight.data.to(device), num_bits=b)
 
             module._active_bit = bit_widths[0]  # default
             module._bit_choices = bit_widths
