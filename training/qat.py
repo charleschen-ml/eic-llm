53fc6076fdd8724ee132ac67b32f1c1e246c8d3d
charleschen-ml <charleschen0305@gmail.com>
2025-06-29 22:53:23 -0700

    a

--- training/qat.py
+++ training/qat.py
@@ -281,7 +281,7 @@ def main(script_args, training_args, model_args):
     # Save and push to hub
     trainer.save_model(training_args.output_dir)
     if USE_BITWISE_LORA:
-        torch.save(model.base_model.model.state_dict(), bitwise_lora_adapter_path)
+        torch.save(model.base_model.state_dict(), bitwise_lora_adapter_path)
     if training_args.push_to_hub:
         trainer.push_to_hub(dataset_name=script_args.dataset_name)
 
