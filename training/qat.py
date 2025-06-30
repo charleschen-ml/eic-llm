86f15cdd66b911a1cb6795a18d71bc38cea7ad9c
charleschen-ml <charleschen0305@gmail.com>
2025-06-30 08:33:02 -0700

    a

--- training/qat.py
+++ training/qat.py
@@ -283,7 +283,7 @@ def main(script_args, training_args, model_args):
     if USE_BITWISE_LORA:
         print(type(model))                         # should show PeftModel
         print(type(model.base_model))             # should show PeftModelForCausalLM
-        print(type(model.base_model.model))       # should show GPT2LMHeadModel
+        # print(type(model.base_model.model))       # should show GPT2LMHeadModel
         torch.save(model.state_dict(), bitwise_lora_adapter_path)
     if training_args.push_to_hub:
         trainer.push_to_hub(dataset_name=script_args.dataset_name)
