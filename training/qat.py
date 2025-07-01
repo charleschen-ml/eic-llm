23648d26abaa054fe942a50408503303eee73c81
charleschen-ml <charleschen0305@gmail.com>
2025-07-01 14:57:41 -0700

    a

--- training/qat.py
+++ training/qat.py
@@ -204,7 +204,9 @@ def add_bitwise_lora_adapters(model, bit_widths=[4, 8, 16]):
                 return output
 
             # Replace original forward function
-            module.forward = forward_with_quant_and_lora.__get__(module, nn.Linear)
+            # module.forward = forward_with_quant_and_lora.__get__(module, nn.Linear)
+            module.forward = forward_with_quant_and_lora.__get__(module, type(module))
+
 
 def set_random_bitwidths(model, bit_choices=[4, 8]):
     for name, module in model.named_modules():
