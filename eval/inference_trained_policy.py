42a2b04d5274533446ae5d46adc8abeabd584f78
charleschen-ml <charleschen0305@gmail.com>
2025-07-03 12:59:57 -0700

    a

--- eval/inference_trained_policy.py
+++ eval/inference_trained_policy.py
@@ -71,10 +71,7 @@ BIT_CHOICES = [8, 16] # bit choices for LoRA. Needs to match training/qat.py
 config1 = {f"transformer.h.{i}": 4 if i % 2 == 0 else 8 for i in range(12)}  # for 12 layers
 config2 = {f"transformer.h.{i}": 4 for i in range(12)}
 config3 = {f"transformer.h.{i}": 8 for i in range(12)}
-config4 = {f"transformer.h.11": 8}
-config5 = {f"transformer.h.11": 4}
-config6 = {f"transformer.h.11": 16}
-config7 = {f"transformer.h.6": 4}
+config4 = {f"transformer.h.6": 8}
 INF_BIT_CONFIG = config4
 
 # Load validation examples from JSON
