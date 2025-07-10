9bcd0a7bbacd8ab98278148fe2d09b684ea4bd0c
charleschen-ml <charleschen0305@gmail.com>
2025-07-10 11:11:19 -0700

    remove peft_config arg in trainer just to be safe

--- training/qat.py
+++ training/qat.py
@@ -479,7 +479,7 @@ def main(script_args, training_args, model_args, qat_args):
         train_dataset=train_dataset, # dataset[script_args.dataset_train_split],
         eval_dataset=eval_dataset, # dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None,
         processing_class=tokenizer,
-        peft_config=get_peft_config(model_args),
+        # peft_config=get_peft_config(model_args), # shouldn't be used but removing just to be sure
         callbacks = callbacks
     )
 
