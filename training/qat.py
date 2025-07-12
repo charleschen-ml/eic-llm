f496f55a11245fa4ac887247da1b327a9f5ab3a0
charleschen-ml <charleschen0305@gmail.com>
2025-07-12 10:38:42 -0700

    a

--- training/qat.py
+++ training/qat.py
@@ -197,8 +197,8 @@ def add_bitwise_lora_adapters(model, bit_widths, quant_layers):
                 input = input[0] if isinstance(input, tuple) else input # to remove
 
                 # Compute base output (no lora, using base, quantized weights)
-                # weight = self._quantized_weights[self._active_bit] # load quantized weights
-                weight = self.weight # load base weights
+                weight = self._quantized_weights[self._active_bit] # load quantized weights
+                # weight = self.weight # load base weights
                 if weight.shape[1] != input.shape[-1]: # transpose matrix for special layers
                     weight = weight.T
                 bias = self.bias # load bias
